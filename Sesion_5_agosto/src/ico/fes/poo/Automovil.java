82009ec495a256a7a159d775d236ade9468020c8
JosueVillagomezRaso <josueraso20@aragon.unam.mx>
2020-08-07 17:45:13 -0500

    Clase 5 de agosto

--- Sesion_5_agosto/src/ico/fes/poo/Automovil.java
+++ Sesion_5_agosto/src/ico/fes/poo/Automovil.java
@@ -9,13 +9,55 @@ package ico.fes.poo;
  *
  * @author Josuel
  */
-public class Automovil {
-    public String marca;
-    public String subMarca;
-    public int modelo;
-    public String color;
+public class Automovil{
+    private String marca;
+    private String subMarca;
+    private int modelo;
+    private String color;
     
-    public void encender(){
+    //metodos de acceso
+    //Modelo
+    public void setModelo(int mod){
+        if(mod >= 1914 && mod <=2020){
+            this.modelo=mod;
+        }
+        else{
+            System.out.println("Ese modelo esta fuera de rango");
+        }
+    }
+    
+    public int getModelo(){
+        return this.modelo;
+    }
+   
+   //Marca
+    public void setMarca(String marca){
+        this.marca=marca;
+    }
+    
+    public String getMarca(){
+        return this.marca;
+    }
+    
+    //SubMarca
+    public void setSubMarca(String subMarca){
+        this.subMarca=subMarca;
+    }
+    
+    public String getSubMarca(){
+        return this.subMarca;
+    }
+    
+    //Color
+    public void setColor(String color){
+        this.color=color;
+    }
+    
+    public String getColor(){
+        return this.color;
+    }
+    
+       public void encender(){
         System.out.println("Encendiendo el auto");
     }
     public void acelerar(){
@@ -24,8 +66,17 @@ public class Automovil {
     
     public float avanzar(int metros){
         float reales=0.0f;
-        reales = (float) Math.random()*100.0f;
+        reales = (float) Math.random()*(float)metros;
         System.out.println("Avanzando el auto " + this.marca);
         return reales;
     }
+
+    @Override
+    public String toString() {
+        return "Marca: " + this.marca + ", SubMarca: " + this.subMarca + ", Modelo: " + this.modelo + ", Color: " + this.color;
+    }
+    
+    
+    
+    
 }
--- Sesion_5_agosto/src/sesion_5_agosto/Sesion_5_agosto.java
+++ Sesion_5_agosto/src/sesion_5_agosto/Sesion_5_agosto.java
@@ -19,11 +19,43 @@ public class Sesion_5_agosto {
     public static void main(String[] args) {
         // TODO code application logic here
         Automovil miCarro =  new Automovil();
-        miCarro.marca="Vw";
-        miCarro.marca="Golf";
-        miCarro.modelo=1990;
-        miCarro.color="Gris";
-        miCarro.avanzar(100);
+        //miCarro.marca="VW";
+        //miCarro.subMarca="Golf";
+        //miCarro.modelo=1990;
+        //miCarro.color="Gris";
+        miCarro.setMarca("VW");
+        miCarro.setSubMarca("Golf");
+        miCarro.setModelo(1990);
+        miCarro.setColor("Gris");
+        System.out.println(miCarro);
+        float x = miCarro.avanzar(1000);
+        System.out.println("Avanzados = "+ x);
+                
+        Automovil miCarroDos = new Automovil();
+        //miCarroDos.marca="Ford";
+        //miCarroDos.subMarca="Mustang";
+        //miCarroDos.modelo=2010;
+        //miCarroDos.color="Amarillo";
+        miCarroDos.setMarca("Ford");
+        miCarroDos.setSubMarca("Mustang");
+        miCarroDos.setModelo(2010);
+        miCarroDos.setColor("Amarillo");
+        System.out.println(miCarroDos);
+        System.out.println("Avanzados = " + miCarroDos.avanzar(500) ); 
+                
+        Automovil miCarroTres = new Automovil();
+        //miCarroTres.marca="Accura";
+        //miCarroTres.subMarca="NSX";
+        //miCarroTres.modelo=2013;
+        //miCarroTres.color="Gris";
+        miCarroTres.setMarca("Accura");
+        miCarroTres.setSubMarca("NSX");
+        miCarroTres.setModelo(2013);
+        miCarroTres.setColor("Gris");
+        System.out.println(miCarroTres);
+        System.out.println("Avanzados = " + miCarroTres.avanzar(500) );
+                  
+        System.out.println(miCarro.toString());
     }
     
 }
