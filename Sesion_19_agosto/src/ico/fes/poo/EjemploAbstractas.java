45e69461e0cab6ad670e6008eb84614a50689cb9
JosueVillagomezRaso <josueraso20@aragon.unam.mx>
2020-08-19 17:21:19 -0500

    Clase 19 de agosto

---
+++ Sesion_19_agosto/src/ico/fes/poo/EjemploAbstractas.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes.poo;
+
+/**
+ *
+ * @author Josuel
+ */
+public interface EjemploAbstractas {
+    public abstract void saludar(int veces); //define la estructura del metodo pero no su cuerpo
+    public void despedir(); 
+}
